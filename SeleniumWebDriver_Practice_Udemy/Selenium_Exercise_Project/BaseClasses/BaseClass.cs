e404e5064f4d5105d5d45157060805e5513e3ebd
Yoganjula Reddy Gangarapu <yoganjula.g@hotmail.com>
2021-11-24 16:11:30 +0530

    * Added ChromeOptions, IEOptions and Firefox profile option methods in Base class.

--- SeleniumWebDriver_Practice_Udemy/Selenium_Exercise_Project/BaseClasses/BaseClass.cs
+++ SeleniumWebDriver_Practice_Udemy/Selenium_Exercise_Project/BaseClasses/BaseClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -15,21 +16,48 @@ namespace Selenium_Exercise_Project.BaseClasses
     [TestClass]
     public class BaseClass
     {
+        private static ChromeOptions GetChromeOptions()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("start-maximized");
+
+            return chromeOptions;
+        }
+
+        private static InternetExplorerOptions GetIEOptions()
+        {
+            InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+            internetExplorerOptions.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+            internetExplorerOptions.EnsureCleanSession = true;
+
+            return internetExplorerOptions;
+        }
+
+        private static FirefoxProfile GetFirefoxOptions()
+        {
+            FirefoxProfile firefoxProfile = new FirefoxProfile();
+            FirefoxProfileManager firefoxProfileManager = new FirefoxProfileManager();
+            firefoxProfile = firefoxProfileManager.GetProfile("default");
+
+            return firefoxProfile;
+        }
+
         private static IWebDriver GetChromeDriver()
         {
-            IWebDriver chromeWebDriver = new ChromeDriver();
+            IWebDriver chromeWebDriver = new ChromeDriver(GetChromeOptions());
             return chromeWebDriver;
         }
 
         private static IWebDriver GetIEDriver()
         {
-            IWebDriver ieDriver = new InternetExplorerDriver();
+            IWebDriver ieDriver = new InternetExplorerDriver(GetIEOptions());
             return ieDriver;
         }
 
         private static IWebDriver GetFirefoxDriver()
         {
             IWebDriver firefoxDriver = new FirefoxDriver();
+            //IWebDriver firefoxDriver = new FirefoxDriver(GetFirefoxOptions()));
             return firefoxDriver;
         }
 
