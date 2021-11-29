06475d511a102cb07d5e9a31af912d8067ca0dea
Yoganjula Reddy Gangarapu <yoganjula.g@hotmail.com>
2021-11-29 07:04:11 +0530

    * Identifying web elements By using TagName, ClassName, CssSelector, LinkText, PartialLinkText, Name, Id and XPath. * Used FindElement() and FindElements() methods to identify web elements. * Created TestWebElement.cs file

---
+++ SeleniumWebDriver_Practice_Udemy/Selenium_Exercise_Project/TestScripts/WebElement/TestWebElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using Selenium_Exercise_Project.ComponentHelper;
+using Selenium_Exercise_Project.Settings;
+
+namespace Selenium_Exercise_Project.TestScripts.WebElement
+{
+    [TestClass]
+    public class TestWebElement
+    {
+        [TestMethod]
+        public void GetElement_From_AmazonWebsite()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+
+            try
+            {
+                ObjectRepository.WebDriver.FindElement(By.TagName("Input"));
+                ObjectRepository.WebDriver.FindElement(By.ClassName("celwidget"));
+                ObjectRepository.WebDriver.FindElement(By.CssSelector("#glow-ingress-line2"));
+                ObjectRepository.WebDriver.FindElement(By.LinkText("Amazon Pay"));
+                ObjectRepository.WebDriver.FindElement(By.PartialLinkText("Home &"));
+                ObjectRepository.WebDriver.FindElement(By.Name("field-keywords"));
+                ObjectRepository.WebDriver.FindElement(By.Id("nav-search-submit-button"));
+                ObjectRepository.WebDriver.FindElement(By.XPath("//select[@id='searchDropdownBox']"));
+                //ObjectRepository.WebDriver.FindElement(By.XPath("//input[@id='email_1']"));
+
+                IReadOnlyCollection<IWebElement> inputWebElements =
+                    ObjectRepository.WebDriver.FindElements(By.TagName("input"));
+            }
+            catch (NoSuchElementException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
